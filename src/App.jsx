66a2565213d8057f28b0fea01f5b60a63d8a7d47
aprbq <apiratqc@gmail.com>
2024-10-02 02:04:07 +0700

    โครงสร้าง+ส่วนหัว

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-red-500'>
-      Hello
+    <div className='mt-12 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
+      <div>
+        <div className='sticky top-12 grid grid-rows-[35%_45%_20%] h-screen'>
+          <div className='flex flex-col gap-2'>
+            <div className='text-3xl text-primaryTitle font-semibold'>Apirat Saengarun</div>
+            <div className='text-primaryTitle font-semibold'>Software Engineer - Studies</div>
+            <div className='text-sm w-5/6'>Third-year Computer Engineering student 
+              <br />at Suranaree University of Technology 
+              <br />aspiring to become a Software Engineer.</div>
+            <div>
+              <a href="#">
+                <span className='rounded-md bg-primaryTitle text-gray-100 py-2 px-4'>
+                  View Resume
+                  <span className='rotate-90 inline-block ml-2'>
+                    <FontAwesomeIcon className='animate-bounce' icon={faArrowDown} />
+                  </span>
+                </span>
+              </a>
+            </div>
+          </div>
+          <div>Nav</div>
+          <div className='flex items-end'>Link</div>
+        </div>
+      </div>
+      <div>
+        <div>About</div>
+        <div className='mb-96'>About</div>
+        <div className='mb-96'>Experince</div>
+        <div className='mb-96'>Project</div>
+        <div className='mb-96'>Article</div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primaryTitle: "#181C14", // เพิ่ม # ก่อนค่า HEX
+        primaryContent: "#697565", // เพิ่ม # ก่อนค่า HEX
+        primarySubContent: "#3C3D37", // เพิ่ม # ก่อนค่า HEX
+        primaryBg: "#ECDFCC", // เพิ่ม # ก่อนค่า HEX
+      }
+    },
   },
   plugins: [],
 }
