2d0551713d19ca3eb62f028c904e71c9a737d862
aprbq <apiratqc@gmail.com>
2024-10-31 23:45:27 +0700

    กรอบ experience ยังไม่เสร็จ

--- src/sections/Contact/index.jsx
+++ src/sections/Contact/index.jsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Contact = () => {
     return (
         <div className='flex items-end gap-4 text-2xl'>
-            <FontAwesomeIcon className='hover:scale-125 hover:text-primaryContent transition-all' icon={faFacebook}/>
-            <FontAwesomeIcon className='hover:scale-125 hover:text-primaryContent transition-all' icon={faGithub}/>
+            <FontAwesomeIcon className='cursor-pointer hover:scale-125 hover:text-primaryContent transition-all' icon={faFacebook}/>
+            <FontAwesomeIcon className='cursor-pointer hover:scale-125 hover:text-primaryContent transition-all' icon={faGithub}/>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ export default {
         primaryContent: "#697565", // เพิ่ม # ก่อนค่า HEX
         primarySubContent: "#3C3D37", // เพิ่ม # ก่อนค่า HEX
         primaryBg: "#ECDFCC", // เพิ่ม # ก่อนค่า HEX
+        primaryProject: "#D4C3B2", // เพิ่ม # ก่อนค่า HEX
+        
       }
     },
   },
